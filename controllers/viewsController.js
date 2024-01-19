35043a5771bd16d8f93dee068d570ea4fd24b3da
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-01-19 23:37:21 +0200

    Update viewsController.js

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,19 +1,29 @@
+const { getMovies, getMovie, searchMovies } = require("../services/movieService");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.render("home");
+router.get("/", async (req, res) => {
+    const movies = await getMovies();
+    res.render("home", { movies });
 });
 
 router.get("/about", (req, res) => {
     res.render("about");
 });
 
-router.get("/search", (req, res) => {
-    res.render("search");
-})
+router.get("/search", async (req, res) => {
+    const movies = await searchMovies(req.query);
+    res.render("search", { movies });
+});
 
 router.get("/create", (req, res) => {
     res.render("create");
+});
+
+router.get("/details/:id", async (req, res) => {
+    const movie = await getMovie(Number(req.params.id));
+    movie.ratingStars = "&#x2605; ".repeat(movie.rating).trimEnd();
+    res.render("details", { movie });
 });
 
 router.get("*", (req, res) => {
