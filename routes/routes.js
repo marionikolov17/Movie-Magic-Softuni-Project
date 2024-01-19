b3505875050b6ae228e05ae88588601d839ae884
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-01-19 23:37:43 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const viewsController = require("./../controllers/viewsController");
+const moviesController = require("./../controllers/moviesController");
 
 router.use(viewsController);
+router.use(moviesController);
 
 module.exports = router;
