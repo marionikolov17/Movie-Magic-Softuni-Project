0e326ae4f63b92ac864daee0505d9efeba2fabd2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-01-19 22:30:43 +0200

    Update viewsController.js

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -8,4 +8,16 @@ router.get("/about", (req, res) => {
     res.render("about");
 });
 
+router.get("/search", (req, res) => {
+    res.render("search");
+})
+
+router.get("/create", (req, res) => {
+    res.render("create");
+});
+
+router.get("*", (req, res) => {
+    res.render("404");
+});
+
 module.exports = router;
