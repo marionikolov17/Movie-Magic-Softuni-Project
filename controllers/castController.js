ea15fee603e170c505df38ec088d15aaa19c9fa7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-01 11:56:30 +0200

    Update castController.js

--- controllers/castController.js
+++ controllers/castController.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
+const castService = require("../services/castService");
+
 router.get("/create", (req, res) => {
     res.render("castCreate");
+});
+
+router.post("/create", async (req, res) => {
+    await castService.createCast(req.body);
+
+    res.redirect("/");
 });
 
 module.exports = router;
