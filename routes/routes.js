6beaeb0fece23a20291530f6d7a6f09af6f32f4a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-01 11:14:48 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const viewsController = require("./../controllers/viewsController");
 const moviesController = require("./../controllers/moviesController");
+const castsController = require("./../controllers/castController");
 
 router.use(viewsController);
 router.use("/movies", moviesController);
+router.use("/casts", castsController);
 
 router.get("*", (req, res) => {
     res.render("404");
