92fba7ff4c62d31b3f823402bdfa7b625287171b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-01 11:56:27 +0200

    Update moviesController.js

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const { createMovie, getMovie } = require('../services/movieService');
 
+const { getCasts } = require("../services/castService");
+
 router.get("/create", (req, res) => {
     res.render("create");
 });
@@ -14,6 +16,12 @@ router.get("/:id", async (req, res) => {
     const movie = await getMovie(req.params.id);
     movie.ratingStars = "&#x2605; ".repeat(movie.rating).trimEnd();
     res.render("details", { movie });
+});
+
+router.get("/:id/attach", async (req, res) => {
+    const movie = await getMovie(req.params.id);
+    const casts = await getCasts();
+    res.render("castAttach", { movie, casts });
 });
 
 router.post("/create", async (req, res) => {
