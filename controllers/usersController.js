1ce0e15179560d6bb379ce1571ed2a45558ac376
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-03 23:25:33 +0200

    Add login functionallity

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,27 @@
 const router = require("express").Router();
 
+const userService = require("./../services/userService");
+
 router.get("/login", (req, res) => {
     res.render("users/login");
 });
 
 router.get("/register", (req, res) => {
     res.render("users/register");
+});
+
+router.post("/register", async (req, res) => {
+    await userService.registerUser(req.body);
+
+    res.redirect("/login");
+});
+
+router.post("/login", async (req, res) => {
+    const token = await userService.loginUser(req.body);
+
+    res.cookie("auth", token);
+
+    res.redirect("/");
 });
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,30 @@
+const bcrypt = require('bcrypt');
+
+const jwt = require("./../utils/jwt");
+
 const userModel = require("./../models/User");
 
 const registerUser = (data) => userModel.create(data);
 
+const loginUser = async (data) => {
+    const user = await userModel.findOne({ email: data.email });
+
+    if (!user) {
+        throw new Error("Email or password are incorrect!");
+    }
+
+    const isCorrectPassword = await bcrypt.compare(data.password, user.password);
+
+    if (!isCorrectPassword) {
+        throw new Error("Email or password are incorrect!");
+    }
+
+    const token = await jwt.sign({ _id: user._id, email: user.email }, process.env.SECRET, { expiresIn: "24h" });
+
+    return token;
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
