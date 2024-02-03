ec1454875a479c711be6414925d5e40d687f065f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-03 22:25:41 +0200

    Create usersController

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,10 @@ const router = require("express").Router();
 const viewsController = require("./../controllers/viewsController");
 const moviesController = require("./../controllers/moviesController");
 const castsController = require("./../controllers/castController");
+const usersController = require("./../controllers/usersController");
 
 router.use(viewsController);
+router.use(usersController);
 router.use("/movies", moviesController);
 router.use("/casts", castsController);
 
