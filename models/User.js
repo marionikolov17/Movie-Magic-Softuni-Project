9ec3f022fc30f40e4735051952144eb318f1efba
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-03 23:08:01 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,36 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      unique: true
     },
     password: {
-        type: String,
-        required: true
-    }
+      type: String,
+      required: true,
+    },
+  },
+  {
+    virtuals: {
+      rePassword: {}
+    },
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  console.log(this.rePassword);
+  if (this.password !== this.rePassword) {
+    throw new Error("Passwords must match!");
+  }
+
+  const hashedPassword = await bcrypt.hash(this.password, 12);
+
+  this.password = hashedPassword;
+
+  next();
 });
 
 const userModel = mongoose.model("User", userSchema);
