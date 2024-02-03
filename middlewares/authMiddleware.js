d8ade7e0ac88cd3f62a0234f5c9c0625deb6cfb0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-03 23:38:59 +0200

    Create authMiddleware.js

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("./../utils/jwt");
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.cookies["auth"];
+
+    if (!token) {
+        return next();
+    }
+
+    try {
+        const decoded = await jwt.verify(token, process.env.SECRET);
+
+        req.userId = decoded._id;
+
+        next();
+    } catch (err) {
+        next();
+    }
+}
+
+const isAuth = (req, res, next) => {
+    if (!req.userId) {
+        return res.redirect("/login");
+    }
+
+    next();
+}
+
+module.exports = {
+    authMiddleware,
+    isAuth
+}
