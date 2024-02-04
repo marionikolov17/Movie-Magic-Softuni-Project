5f344fba3c7f4d6864a7368d0cf51b59ac2d52c0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-02-04 12:05:52 +0200

    Implement edit and delete routes

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
 const auth = require("./../middlewares/authMiddleware");
 
-const { createMovie, getMovie, attachCast, getMovieCasts } = require('../services/movieService');
+const { createMovie, getMovie, attachCast, getMovieCasts, editMovie, deleteMovie } = require('../services/movieService');
 
 const { getCasts } = require("../services/castService");
+
+
+const isOwner = async (req, res, next) => {
+    const movie = await getMovie(req.params.id);
+    const isOwner = req.userId == movie.owner;
+
+    if (!isOwner) {
+        return res.redirect(`/movies/${req.params.id}`);
+    }
+
+    req.movie = movie;
+    req.isOwner = isOwner;
+    next();
+}
 
 router.get("/create", auth.isAuth, (req, res) => {
     res.render("create");
@@ -30,15 +44,13 @@ router.get("/:id/attach", auth.isAuth, async (req, res) => {
     res.render("castAttach", { movie, casts });
 });
 
-router.get("/:id/edit", auth.isAuth, async (req, res) => {
-    const movie = await getMovie(req.params.id);
-    const isOwner = req.userId == movie.owner;
+router.get("/:id/edit", auth.isAuth, isOwner, (req, res) => {
+    res.render("movies/edit", { movie: req.movie });
+});
 
-    if (!isOwner) {
-        return res.redirect(`/movies/${req.params.id}`);
-    }
-
-    res.render("movies/edit", { movie });
+router.get("/:id/delete", auth.isAuth, isOwner, async (req, res) => {
+    await deleteMovie(req.params.id);
+    res.redirect("/");
 });
 
 router.post("/:id/attach", auth.isAuth, async (req, res) => {
@@ -50,6 +62,11 @@ router.post("/:id/attach", auth.isAuth, async (req, res) => {
 router.post("/create", auth.isAuth, async (req, res) => {
     await createMovie({...req.body, owner: req.userId});
     res.redirect("/");
+});
+
+router.post("/:id/edit", auth.isAuth, isOwner, async (req, res) => {
+    await editMovie(req.params.id, req.body);
+    res.redirect(`/movies/${req.params.id}`);
 });
 
 module.exports = router;
